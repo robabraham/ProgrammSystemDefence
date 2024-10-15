8b0c58aa155cc9e9cb377ce8c5d07bcc5d84a1c3
robabraham <37072112+robabraham@users.noreply.github.com>
2024-10-15 10:17:00 +0400

    Update gcd.cpp
    Լաբորատոր աշխատանք 1
    Ամենամեծ  ընդհանուր բաժանարար

--- gcd.cpp
+++ gcd.cpp
@@ -1,26 +1,28 @@
-Բաժանարար 
 #include <iostream>
 using namespace std;
 
-
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-} 
+class MathOperations {
+public:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+};
 
 int main() {
-    int num1, num2; 
+    int num1, num2;
+    MathOperations mathOps;
 
-    cout << "Մուտքագրեք երկու ամբողջ թիվ: ";
-    cin >> num1 >> num2; 
+    cout << "Input numbers: ";
+    cin >> num1 >> num2;
 
-    int result = gcd(num1, num2); 
+    int result = mathOps.gcd(num1, num2);
 
-    cout << "Երկու թվերի ամենամեծ ընդհանուր բաժանարարը = " << result << endl; 
+    cout << "Numbers GCD = " << result << endl;
 
-    return 0;
+    return 0;
 }
