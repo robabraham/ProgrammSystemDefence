3fbfb9d53a01b25648076a39a6c9d5a4fbf08d06
robabraham <37072112+robabraham@users.noreply.github.com>
2024-10-15 10:22:30 +0400

    Update encode_decode.cpp
    Լաբորատոր աշխատանք 2
    Կոդավորում-վերծանում

--- encode.cpp
+++ encode.cpp
@@ -2,44 +2,80 @@
 #include <string>
 using namespace std;
 
-int main() {
+class Cipher {
+private:
     char alphabet[26];
     char reverseAlphabet[26];
-    
-    for (int i = 0; i < 26; i++) {
-        alphabet[i] = 'A' + i;
-        reverseAlphabet[i] = 'Z' - i;
+
+public:
+    Cipher() {
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = 'A' + i;
+            reverseAlphabet[i] = 'Z' - i;
+        }
     }
-    
-    cout << "Alphabet: ";
-    for (int i = 0; i < 26; i++) {
-        cout << alphabet[i] << " ";
+
+    void displayAlphabets() const {
+        cout << "Alphabet: ";
+        for (int i = 0; i < 26; i++) {
+            cout << alphabet[i] << " ";
+        }
+        cout << endl;
+
+        cout << "Reverse Alphabet: ";
+        for (int i = 0; i < 26; i++) {
+            cout << reverseAlphabet[i] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-    
-    cout << "Reverse Alphabet: ";
-    for (int i = 0; i < 26; i++) {
-        cout << reverseAlphabet[i] << " ";
+
+    string encode(const string& inputText) const {
+        string encodedText = "";
+        for (char c : inputText) {
+            if (isalpha(c)) {  
+                if (isupper(c)) {
+                    encodedText += reverseAlphabet[c - 'A'];
+                } else {
+                    encodedText += tolower(reverseAlphabet[c - 'a']);  
+                }
+            } else {
+                encodedText += c; 
+            }
+        }
+        return encodedText;
     }
-    cout << endl;
-    
+
+    string decode(const string& encodedText) const {
+        string decodedText = "";
+        for (char c : encodedText) {
+            if (isalpha(c)) {  
+                if (isupper(c)) {
+                    decodedText += alphabet['Z' - c];
+                } else {
+                    decodedText += tolower(alphabet['z' - c]);  
+                }
+            } else {
+                decodedText += c;  
+            }
+        }
+        return decodedText;
+    }
+};
+
+int main() {
+    Cipher cipher;
+
+    cipher.displayAlphabets();
+
     string inputText;
     cout << "Input Text: ";
     getline(cin, inputText);
 
-    string encodedText = "";
-    for (char c : inputText) {
-        if (isalpha(c)) {  
-            if (isupper(c)) {
-                encodedText += reverseAlphabet[c - 'A']; 
-            } else {
-                encodedText += tolower(reverseAlphabet[c - 'a']);  
-            } 
-        } else {
-            encodedText += c;
-        }
-    }
-    cout << "Encode text: " << encodedText << endl;
+    string encodedText = cipher.encode(inputText);
+    cout << "Encoded text: " << encodedText << endl;
+
+    string decodedText = cipher.decode(encodedText);
+    cout << "Decoded text: " << decodedText << endl;
 
     return 0;
 }
