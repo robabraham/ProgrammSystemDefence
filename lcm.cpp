fa78a404d354ca914b9dc93260319e4dd3feb941
robabraham <37072112+robabraham@users.noreply.github.com>
2024-10-15 10:13:54 +0400

    Update lcm.cpp
    Լաբորատոր  աշխատանք 1 
    Ամենափոքր  ընդհանուր բազմապատիկ

--- lcm.cpp
+++ lcm.cpp
@@ -1,32 +1,34 @@
-Բազմապատիկ 
-
 #include <iostream>
 using namespace std;
 
+class MathOperations {
+public:
+    
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 
-int gcd(int a, int b) {
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
-}
-
-
-int lcm(int a, int b) {
-    return (a * b) / gcd(a, b);
-} 
+    
+    int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+};
 
 int main() {
-    int num1, num2; 
+    int num1, num2;
+    MathOperations mathOps;
 
-    cout << "Մուտքագրեք երկու ամբողջ թիվ: ";
-    cin >> num1 >> num2; 
+    cout << "Input numbers: ";
+    cin >> num1 >> num2;
 
-    int result = lcm(num1, num2); 
+    int result = mathOps.lcm(num1, num2);
 
-    cout << "Երկու թվերի ամենափոքր ընդհանուր բազմապատիկը = " << result << endl; 
+    cout << "Numbers LCM = " << result << endl;
 
-    return 0;
+    return 0;
 }
